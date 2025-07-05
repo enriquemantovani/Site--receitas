44906228da7599e6c8fceb97dc512c449c61219e
Enrique Mantovani <mantovanienrique2211@gmail.com>
2025-07-05 17:56:36 -0300

    feat: adiciona estilos ao header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,21 +2,23 @@ import Link from "next/link"
 
 export default function Header() {
     return (
-        <header>
-            <Link href="/"> Receitas Deliciosas</Link>
+        <header className= "w-full bg-white text-black py-4">
+            <div className= " flex justify-between conteiner mx-auto">
+            <Link  className= "text-xl font-bold hover:scale-105 transition-all" href="/"> Receitas Deliciosas</Link>
 
-            <nav>
-                <Link href="/">
-                Inicio
-                </Link>
+                <nav className="flex gap-6">
+                  <Link className="hover:text-orange-500 transition-colors " href="/">
+                    Inicio
+                  </Link>
 
-                <Link href="/receitas">
-                Receitas
-                </Link>
+                    <Link className="hover:text-orange-500 transition-colors " href="/receitas">
+                        Receitas
+                    </Link>
 
 
 
-            </nav>
+                </nav>
+            </div>
         </header>
         
     )
