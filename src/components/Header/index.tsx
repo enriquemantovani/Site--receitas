adb868a5dcdce732a8dec2c9422367b0555621cd
Enrique Mantovani <mantovanienrique2211@gmail.com>
2025-07-05 17:35:25 -0300

    feat: estrutura do header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link"
+
+export default function Header() {
+    return (
+        <header>
+            <Link href="/"> Receitas Deliciosas</Link>
+
+            <nav>
+                <Link href="/">
+                Inicio
+                </Link>
+
+                <Link href="/receitas">
+                Receitas
+                </Link>
+
+
+
+            </nav>
+        </header>
+        
+    )
+
+}
