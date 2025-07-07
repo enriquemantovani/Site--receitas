e5c4ba6f3bca7ef41411ffc307534b07ef9bf86e
Enrique Mantovani <mantovanienrique2211@gmail.com>
2025-07-07 15:58:17 -0300

    feat: estrutura da secao Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
   <main className="flex-grow">
-    <h1>Tela Inicial</h1>
+    <div className= "container mx-auto">
+      {/* seção Hero */}
+
+      <section>
+        <h1>Receitas Deliciosas</h1>
+        <p>Descubra receitas simples e saborosas para todas as ocasiões</p>
+
+        <Link href= "/receitas">
+        Ver Todas as receitas</Link>
+      </section>
+
+    </div>
   </main>
   );
 }
