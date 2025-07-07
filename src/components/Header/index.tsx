6e037db3c0389e8b61788656accbce490ea58c62
Enrique Mantovani <mantovanienrique2211@gmail.com>
2025-07-07 16:13:42 -0300

    feat: adiciona estilos na secao Hero

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,8 @@ import Link from "next/link"
 
 export default function Header() {
     return (
-        <header className= "w-full bg-white text-black py-4">
-            <div className= " flex justify-between conteiner mx-auto">
+        <header className= "w-full bg-white text-black py-4 border-b border-slate-200 drop-shadow-lg ">
+            <div className= " flex justify-between container mx-auto">
             <Link  className= "text-xl font-bold hover:scale-105 transition-all" href="/"> Receitas Deliciosas</Link>
 
                 <nav className="flex gap-6">
