dfd6b6427a1ca0d8685b6ef1b6a585abdeec658e
Enrique Mantovani <mantovanienrique2211@gmail.com>
2025-07-07 22:36:54 -0300

    feat: cards de receita

--- src/app/receitas/page.tsx
+++ src/app/receitas/page.tsx
@@ -1,12 +1,16 @@
+import RecipeCard from "@/components/RecipeCard";
+import { recipes} from "@/lib/data";
+
 export default function ReceitasPage(){
     return(
         <main className="flex-grow py-8">
             <div className="container mx-auto">
                 <h1 className="text-3xl font-bold">Todas as Receitas</h1>
 
-
-                <div>
-                    {/*arrays de cards de receita*/}
+                <div className="grid grid-cols-3 gap-8 mt-8">
+                    {recipes.map((recipe) => (
+                    <RecipeCard/>
+                    ))}
                 </div>
             </div>
         </main>
