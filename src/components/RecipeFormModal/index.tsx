4ad891561fb5ad2bae6ffe2c16d7b3f7002cfa4f
Enrique Mantovani <mantovanienrique2211@gmail.com>
2025-07-15 10:34:53 -0300

    feat: estrutura das listas dinamicas

--- src/components/RecipeFormModal/index.tsx
+++ src/components/RecipeFormModal/index.tsx
@@ -28,7 +28,7 @@ export default function RecipeFormModal({ isOpen, onClose }: RecipeFormModalProp
     }
 
 
-    const inputStyle = "p-2 border border-zonc-200 rounded-md"
+    const inputStyle = "p-2 border border-zinc-200 rounded-md flex-grow"
 
 
     return(
@@ -91,8 +91,41 @@ export default function RecipeFormModal({ isOpen, onClose }: RecipeFormModalProp
                             </div>
                         </div>
 
+                        {/*Lista de ingredientes*/}
+                        <div className="flex flex-col gap-1">
+                            <label htmlFor="ingredients">ingredientes</label>
+                            <div className="flex flex-col gap-1">                            
+                                {/*Conteudo*/}
+                                 <div className="flex gap-2">
+                                <input id="ingredients" type="text" className={inputStyle}/>
+                                <button type="button" className="bg-white border border-zinc-300 rounded-md hover:bg-gray-100 transition-colors px-4 py-2 
+                                    font medium">Remover</button>
+                                </div>
+                                <button type="button" className="bg-white border border-zinc-300 rounded-md hover:bg-gray-100 transition-colors px-4 py-2 
+                            font medium w-fit">Adicionar ingrediente</button>
+                            </div>
+                        </div>
+
+                        {/*Instruções*/}
+                        <div className="flex flex-col gap-1">
+                            <div className="flex flex-col gap-1">                            
+                                {/*Conteudo*/}
+                                <label htmlFor="instructions">instruções</label>
+                                <div className="flex gap-2">
+                                    <textarea id="instructions" className={inputStyle}/>
+                                    <button type="button" className="bg-white border border-zinc-300 rounded-md hover:bg-gray-100 transition-colors px-4 py-2 
+                                    font medium h-fit">Remover</button>
+                                </div>
+
+                                <button type="button" className="bg-white border border-zinc-300 rounded-md hover:bg-gray-100 transition-colors px-4 py-2 
+                            font medium w-fit">Adicionar instrução</button>
+                            </div>
+                        </div>
+
+
                         <div className="felx gap-2 self-end">
-                            <button type="button" onClick={onClose} className="bg-white border-zinc-300 rounded-md hover:bg-gray-100 transition-colors px-4 py-2 font medium">Cancelar</button>
+                            <button type="button" onClick={onClose} className="bg-white border-zinc-300 rounded-md hover:bg-gray-100 transition-colors px-4 py-2 
+                            font medium">Cancelar</button>
                             <button type="submit" className="bg-black rounded-md text-white hover:bg-gray-800 transition-colors px-4 py-2 font medium" >Criar receita</button>
                         </div>
 
